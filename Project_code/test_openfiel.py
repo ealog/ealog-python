5501cf76e44e555c2a708e17df821e7b55fed829
ealog <yang.liu@ealog.com.cn>
2020-10-26 16:02:17 +0800

    add test file

---
+++ Project_code/test_openfiel.py
@@ -0,0 +1,13 @@
+
+file_name = 'pi_digits.txt'
+
+with open(file_name) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+    # contents = file_object.read()
+
+# print(contents.rstrip())
+
+
+
+
