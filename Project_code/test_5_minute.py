d2f202402a55a18c373f828d51794c1cb45f8d0b
ealog <yang.liu@ealog.com.cn>
2020-12-09 09:10:25 +0800

    add

--- Project_code/test_5_minute.py
+++ Project_code/test_5_minute.py
@@ -43,7 +43,11 @@ timeout = conf.getint('METER_INIT','timeout')
 
 # 定义发生读取错误时，发送邮件至那个邮箱地址
 
-email_addr = 
+mail_host=conf.get('EMAIL_CONFIG','email_smtp') #设置服务器
+mail_user=conf.get('EMAIL_CONFIG','email_sender') #用户名
+mail_pass=conf.get('EMAIL_CONFIG','email_pwd') #口令 
+sender = conf.get('EMAIL_CONFIG','email_sender')
+receivers = conf.get('EMAIL_CONFIG','email_receiver') 
 
 # 此条定义无意义，使用字典，定义每个设备的上次数据 key 代表设备地址 values 存储设备上次数据uart_inst
 lt_impep={}
@@ -161,122 +165,138 @@ def connect_uart(modbus_set,baudrate=9600,timeout=1):
 
 # 设备数据读取，并返回字典，key 为设备 ，value 为 返回数据列表
 def data_read(inst):
-    
-    # 重复执行数据读取写入'
-    
+    try:    
+        # 重复执行数据读取写入'
+        
 
-    cur_year=datetime.datetime.now().year
-    cur_month=datetime.datetime.now().month
-    cur_day=datetime.datetime.now().day
-    cur_hour=datetime.datetime.now().hour
-    cur_minute=datetime.datetime.now().minute
-    time_now=datetime.datetime.now()
-    cur_date=datetime.date.today()
-    # 读取季度 格式为：2020Q1-2020Q4
-    quarter = (cur_date.month-1) // 3 + 1
-    year_quarter='{}Q{}'.format(cur_date.year, quarter)
-    
-    day_week = datetime.datetime.now().isoweekday()
-    week_count=datetime.datetime.now().isocalendar()[1]
-    
-    meter_addr=inst.read_register(46,0,3,signed=False)
-    # Uab=(inst.read_float(8192,3,2,byteorder=0))*0.1
-    # time.sleep(SEC)
-    # Ubc=(inst.read_float(8194,3,2,byteorder=0))*0.1
-    # time.sleep(SEC)
-    # Uca=(inst.read_float(8196,3,2,byteorder=0))*0.1
-    # time.sleep(SEC)
-    Ua=(inst.read_float(8198,3,2,byteorder=0))*0.1
-    # time.sleep(SEC)
-    Ub=(inst.read_float(8200,3,2,byteorder=0))*0.1
-    # time.sleep(SEC)
-    Uc=(inst.read_float(8202,3,2,byteorder=0))*0.1
-    # time.sleep(SEC)
-    Ia=(inst.read_float(8204,3,2,byteorder=0))*0.001*120
-    # time.sleep(SEC)
-    Ib=(inst.read_float(8206,3,2,byteorder=0))*0.001*120
-    # time.sleep(SEC)
-    Ic=(inst.read_float(8208,3,2,byteorder=0))*0.001*120
-    # time.sleep(SEC)
-    Pt=(inst.read_float(8210,3,2,byteorder=0))*0.1*120/1000
-    # time.sleep(SEC)
-    Pa=(inst.read_float(8212,3,2,byteorder=0))*0.1*120/1000
-    # time.sleep(SEC)
-    Pb=(inst.read_float(8214,3,2,byteorder=0))*0.1*120/1000
-    # time.sleep(SEC)
-    Pc=(inst.read_float(8216,3,2,byteorder=0))*0.1*120/1000
-    # time.sleep(SEC)
-    # Qt=(inst.read_float(8218,3,2,byteorder=0))*0.1
-    # time.sleep(SEC)
-    # Qa=(inst.read_float(8220,3,2,byteorder=0))*0.1
-    # time.sleep(SEC)
-    # Qb=(inst.read_float(8222,3,2,byteorder=0))*0.1
-    # time.sleep(SEC)
-    # Qc=(inst.read_float(8224,3,2,byteorder=0))*0.1
-    # time.sleep(SEC)
-    PFt=(inst.read_float(8234,3,2,byteorder=0))*0.001
-    # time.sleep(SEC)
-    PFa=(inst.read_float(8236,3,2,byteorder=0))*0.001
-    # time.sleep(SEC)
-    PFb=(inst.read_float(8238,3,2,byteorder=0))*0.001
-    # time.sleep(SEC)
-    PFc=(inst.read_float(8240,3,2,byteorder=0))*0.001
-    # time.sleep(SEC)
-    Freq=(inst.read_float(8260,3,2,byteorder=0))*0.01
-    # time.sleep(SEC)
-    ImpEp=inst.read_float(4126,3,2,byteorder=0)*120
-    # time.sleep(SEC)
-    ExpEp=inst.read_float(4136,3,2,byteorder=0)*120
-    # time.sleep(SEC)
-    Q1Eq=inst.read_float(4146,3,2,byteorder=0)*120
-    # time.sleep(SEC)
-    Q2Eq=inst.read_float(4156,3,2,byteorder=0)*120
-    # time.sleep(SEC)
-    Q3Eq=inst.read_float(4166,3,2,byteorder=0)*120
-    # time.sleep(SEC)
-    Q4Eq=inst.read_float(4176,3,2,byteorder=0)*120
-    # time.sleep(SEC)
-    # print("v is :"+str(Uab))
-    # print("v is :"+str(Ubc))
-    # print("v is :"+str(Uca))
-    print("Ua is :"+str(Ua))
-    print("Ub is :"+str(Ub))
-    print("Uc is :"+str(Uc))
-    print("Ia is :"+str(Ia))
-    print("Ib is :"+str(Ib))
-    print("Ic is :"+str(Ic))
-    print("Pt is :"+str(Pt))
-    print("Pa is :"+str(Pa))
-    print("Pb is :"+str(Pb))
-    print("Pc is :"+str(Pc))
-    # print("v is :"+str(Qt))
-    # print("v is :"+str(Qadatabase_instreq))
-    print("ImpEp is :"+str(ImpEp))
-    print("ExpEp is :"+str(ExpEp))
-    print("Q1Eq is :"+str(Q1Eq))
-    print("Q2Eq is :"+str(Q2Eq))
-    print("Q3Eq is :"+str(Q3Eq))
-    print("Q4Eq is :"+str(Q4Eq))
-    print("Meter_addr is :"+str(meter_addr))
-    print('lt_impep',lt_impep[str(meter_addr)])
-    print('ImpEp:',ImpEp)
-    ImpEp_increase = ImpEp - lt_impep[str(meter_addr)]
-    print('ImpEp_increase:',ImpEp_increase)
-    # 写入数据库字段值列表
-    data_list = []
-    
-    H_L_M=f_g_p1(cur_hour,cur_minute)
-    
-    data_list.append((meter_addr,time_now,cur_date,year_quarter,week_count,day_week,cur_year,cur_month,cur_day,cur_hour,cur_minute,H_L_M,Ua,Ub,Uc,Ia,Ib,Ic,Pt,Pa,Pb,Pc,PFt,PFa,PFb,PFc,Freq,ImpEp,ImpEp_increase,ExpEp,Q1Eq,Q2Eq,Q3Eq,Q4Eq))
+        cur_year=datetime.datetime.now().year
+        cur_month=datetime.datetime.now().month
+        cur_day=datetime.datetime.now().day
+        cur_hour=datetime.datetime.now().hour
+        cur_minute=datetime.datetime.now().minute
+        time_now=datetime.datetime.now()
+        cur_date=datetime.date.today()
+        # 读取季度 格式为：2020Q1-2020Q4
+        quarter = (cur_date.month-1) // 3 + 1
+        year_quarter='{}Q{}'.format(cur_date.year, quarter)
+        
+        day_week = datetime.datetime.now().isoweekday()
+        week_count=datetime.datetime.now().isocalendar()[1]
+        
+        meter_addr=inst.read_register(46,0,3,signed=False)
+        # Uab=(inst.read_float(8192,3,2,byteorder=0))*0.1
+        # time.sleep(SEC)
+        # Ubc=(inst.read_float(8194,3,2,byteorder=0))*0.1
+        # time.sleep(SEC)
+        # Uca=(inst.read_float(8196,3,2,byteorder=0))*0.1
+        # time.sleep(SEC)
+        Ua=(inst.read_float(8198,3,2,byteorder=0))*0.1
+        # time.sleep(SEC)
+        Ub=(inst.read_float(8200,3,2,byteorder=0))*0.1
+        # time.sleep(SEC)
+        Uc=(inst.read_float(8202,3,2,byteorder=0))*0.1
+        # time.sleep(SEC)
+        Ia=(inst.read_float(8204,3,2,byteorder=0))*0.001*120
+        # time.sleep(SEC)
+        Ib=(inst.read_float(8206,3,2,byteorder=0))*0.001*120
+        # time.sleep(SEC)
+        Ic=(inst.read_float(8208,3,2,byteorder=0))*0.001*120
+        # time.sleep(SEC)
+        Pt=(inst.read_float(8210,3,2,byteorder=0))*0.1*120/1000
+        # time.sleep(SEC)
+        Pa=(inst.read_float(8212,3,2,byteorder=0))*0.1*120/1000
+        # time.sleep(SEC)
+        Pb=(inst.read_float(8214,3,2,byteorder=0))*0.1*120/1000
+        # time.sleep(SEC)
+        Pc=(inst.read_float(8216,3,2,byteorder=0))*0.1*120/1000
+        # time.sleep(SEC)
+        # Qt=(inst.read_float(8218,3,2,byteorder=0))*0.1
+        # time.sleep(SEC)
+        # Qa=(inst.read_float(8220,3,2,byteorder=0))*0.1
+        # time.sleep(SEC)
+        # Qb=(inst.read_float(8222,3,2,byteorder=0))*0.1
+        # time.sleep(SEC)
+        # Qc=(inst.read_float(8224,3,2,byteorder=0))*0.1
+        # time.sleep(SEC)
+        PFt=(inst.read_float(8234,3,2,byteorder=0))*0.001
+        # time.sleep(SEC)
+        PFa=(inst.read_float(8236,3,2,byteorder=0))*0.001
+        # time.sleep(SEC)
+        PFb=(inst.read_float(8238,3,2,byteorder=0))*0.001
+        # time.sleep(SEC)
+        PFc=(inst.read_float(8240,3,2,byteorder=0))*0.001
+        # time.sleep(SEC)
+        Freq=(inst.read_float(8260,3,2,byteorder=0))*0.01
+        # time.sleep(SEC)
+        ImpEp=inst.read_float(4126,3,2,byteorder=0)*120
+        # time.sleep(SEC)
+        ExpEp=inst.read_float(4136,3,2,byteorder=0)*120
+        # time.sleep(SEC)
+        Q1Eq=inst.read_float(4146,3,2,byteorder=0)*120
+        # time.sleep(SEC)
+        Q2Eq=inst.read_float(4156,3,2,byteorder=0)*120
+        # time.sleep(SEC)
+        Q3Eq=inst.read_float(4166,3,2,byteorder=0)*120
+        # time.sleep(SEC)
+        Q4Eq=inst.read_float(4176,3,2,byteorder=0)*120
+        # time.sleep(SEC)
+        # print("v is :"+str(Uab))
+        # print("v is :"+str(Ubc))
+        # print("v is :"+str(Uca))
+        print("Ua is :"+str(Ua))
+        print("Ub is :"+str(Ub))
+        print("Uc is :"+str(Uc))
+        print("Ia is :"+str(Ia))
+        print("Ib is :"+str(Ib))
+        print("Ic is :"+str(Ic))
+        print("Pt is :"+str(Pt))
+        print("Pa is :"+str(Pa))
+        print("Pb is :"+str(Pb))
+        print("Pc is :"+str(Pc))
+        # print("v is :"+str(Qt))
+        # print("v is :"+str(Qadatabase_instreq))
+        print("ImpEp is :"+str(ImpEp))
+        print("ExpEp is :"+str(ExpEp))
+        print("Q1Eq is :"+str(Q1Eq))
+        print("Q2Eq is :"+str(Q2Eq))
+        print("Q3Eq is :"+str(Q3Eq))
+        print("Q4Eq is :"+str(Q4Eq))
+        print("Meter_addr is :"+str(meter_addr))
+        print('lt_impep',lt_impep[str(meter_addr)])
+        print('ImpEp:',ImpEp)
+        ImpEp_increase = ImpEp - lt_impep[str(meter_addr)]
+        print('ImpEp_increase:',ImpEp_increase)
+        # 写入数据库字段值列表
+        data_list = []
+        
+        H_L_M=f_g_p1(cur_hour,cur_minute)
+        
+        data_list.append((meter_addr,time_now,cur_date,year_quarter,week_count,day_week,cur_year,cur_month,cur_day,cur_hour,cur_minute,H_L_M,Ua,Ub,Uc,Ia,Ib,Ic,Pt,Pa,Pb,Pc,PFt,PFa,PFb,PFc,Freq,ImpEp,ImpEp_increase,ExpEp,Q1Eq,Q2Eq,Q3Eq,Q4Eq))
 
-    # 更新lt_impep[meter_addr]数量为最后一次电量数，为算增加电量数据做准备
-    
+        # 更新lt_impep[meter_addr]数量为最后一次电量数，为算增加电量数据做准备
+        
 
-    meter_data = {meter_addr:data_list}
-    print("线程数据为： ",meter_data)
+        meter_data = {meter_addr:data_list}
+        print("线程数据为： ",meter_data)
 
-    return meter_data
-
+        return meter_data
+    except:
+        message = MIMEText(inst, 'plain', 'utf-8')
+        message['From'] = Header("菜鸟教程", 'utf-8')
+        message['To'] =  Header("测试", 'utf-8')
+        
+        subject = '设备读取错误！'
+        message['Subject'] = Header(subject, 'utf-8')
+        
+        
+        try:
+            smtpObj = smtplib.SMTP() 
+            smtpObj.connect(mail_host, 25)    # 25 为 SMTP 端口号
+            smtpObj.login(mail_user,mail_pass)  
+            smtpObj.sendmail(sender, receivers, message.as_string())
+            print ("邮件发送成功")
+        except smtplib.SMTPException:
+            print ("Error: 无法发送邮件")
 def data_write(database_inst,uart_data):
     print(lt_impep)
     try:    
